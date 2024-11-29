e9c342a3f5766daefe66d6830a51f93daa646310
yuumenye <yumenye@proton.me>
2024-11-30 00:56:42 +0300

    Save graphs to log/

--- draw.cpp
+++ draw.cpp
@@ -13,7 +13,7 @@ static void write_edges(FILE *file, struct node *node);
 /* draw tree using graphviz */
 void tree_draw(struct tree *tree)
 {
-        const int namelen = 20;
+        const int namelen = 25;
         char filename[namelen] = "";
         generate_filename(filename, namelen);
 
@@ -27,14 +27,14 @@ void tree_draw(struct tree *tree)
 static void generate_filename(char *filename, int namelen)
 {
         static int filenum = 0;
-        snprintf(filename, namelen, "tree%03d.dot", filenum++);
+        snprintf(filename, namelen, "log/tree%03d.dot", filenum++);
 }
 
 static void compile_dot(char *filename)
 {
         const int cmdlen = 100;
         char cmd[cmdlen] = "";
-        snprintf(cmd, cmdlen, "dot -T png %s -o %.7s.png", filename, filename);
+        snprintf(cmd, cmdlen, "dot -T png %s -o %.11s.png", filename, filename);
         system(cmd);
 }
 
