0c0990a7635628fb658de5b03c629ef2de10d677
yuumenye <yumenye@proton.me>
2025-01-25 12:48:39 +0300

    Refactor definitions

--- akinator.cpp
+++ akinator.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "akinator.h"
 #include "string.h"
@@ -6,6 +7,7 @@
 #include "stack.h"
 
 static void print_def(struct tree *tree);
+static struct node *get_search_node(struct tree *tree);
 static void save_node_path(struct node *node, struct stack *stk);
 static void print_node_path(struct stack *stk);
 
@@ -23,15 +25,7 @@ void run_akinator()
 
 static void print_def(struct tree *tree)
 {
-        char key[keylen] = "";
-        scanf("%s", key);
-
-        struct node *node = {};
-        node_find(tree->root, &node, key);
-        if (!node) {
-                printf("Couldn't find node \"%s\"\n", key);
-                return;
-        }
+        struct node *node = get_search_node(tree);
 
         const int stk_capacity = 100;
         struct stack stk = {};
@@ -44,6 +38,22 @@ static void print_def(struct tree *tree)
         stack_dtor(&stk);
 }
 
+static struct node *get_search_node(struct tree *tree)
+{
+        struct node *node = NULL;
+        while (!node) {
+                char key[keylen] = "";
+                if (scanf("%s", key) == EOF)
+                        exit(0);
+
+                node_find(tree->root, &node, key);
+
+                if (!node)
+                        printf("Couldn't find node \"%s\"\n", key);
+        }
+        return node;
+}
+
 static void save_node_path(struct node *node, struct stack *stk)
 {
         while (node) {
@@ -54,8 +64,8 @@ static void save_node_path(struct node *node, struct stack *stk)
 
 static void print_node_path(struct stack *stk)
 {
-        struct node *node1 = {};
-        struct node *node2 = {};
+        struct node *node1 = NULL;
+        struct node *node2 = NULL;
 
         stack_pop(stk, &node1);
         while (stk->size > 0) {
